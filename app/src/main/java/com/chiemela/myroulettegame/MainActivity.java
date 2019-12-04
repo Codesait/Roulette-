51b262f2e87227fa3233fc01375b494ab493d8f6
root <root@localhost.localdomain>
2019-12-04 08:02:36 +0100

    final

--- app/src/main/java/com/chiemela/myroulettegame/MainActivity.java
+++ app/src/main/java/com/chiemela/myroulettegame/MainActivity.java
@@ -2,6 +2,10 @@ package com.chiemela.myroulettegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +14,7 @@ import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +46,66 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.spinBtn)
+    public void spin(View v) {
+        degreeOld = degree % 360;
+        // we calculate random angle for rotation of our wheel
+        degree = RANDOM.nextInt(360) + 720;
+        // rotation effect on the center of the wheel
+        RotateAnimation rotateAnim = new RotateAnimation(degreeOld, degree,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnim.setDuration(3600);
+        rotateAnim.setFillAfter(true);
+        rotateAnim.setInterpolator(new DecelerateInterpolator());
+        rotateAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                // we empty the result text view when the animation start
+                resultTv.setText("");
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // we display the correct sector pointed by the triangle at the end of the rotate animation
+                resultTv.setText(getSector(360 - (degree % 360)));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        // we start the animation
+        wheel.startAnimation(rotateAnim);
+    }
+
+    private String getSector(int degrees) {
+        int i = 0;
+        String text = null;
+
+        do {
+            // start and end of each sector on the wheel
+            float start = HALF_SECTOR * (i * 2 + 1);
+            float end = HALF_SECTOR * (i * 2 + 3);
+
+            if (degrees >= start && degrees < end) {
+                // degrees is in [start;end[
+                // so text is equals to sectors[i];
+                text = sectors[i];
+            }
+
+            i++;
+            // now we can test our Android Roulette Game :)
+            // That's all !
+            // In the second part, you will learn how to add some bets on the table to play to the Roulette Game :)
+            // Subscribe and stay tuned !
+
+        } while (text == null  &&  i < sectors.length);
+
+        return text;
+    }
+
     // sectors of our wheel (look at the image to see the sectors)
     private static final String[] sectors = {"32 red", "15 black",
             "19 red", "4 black", "21 red", "2 black", "25 red", "17 black", "34 red",
@@ -50,5 +115,7 @@ public class MainActivity extends AppCompatActivity {
             "18 red", "29 black", "7 red", "28 black", "12 red", "35 black",
             "3 red", "26 black", "zero"
     };
+
+
 
 }
